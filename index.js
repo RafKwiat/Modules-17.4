8c66415d552cc60a7f3fd07aac19c10e951ff085
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-08-28 20:47:48 +0200

    Commit fs

--- index.js
+++ index.js
@@ -21,28 +21,28 @@ var array = new Array(files);
 });
 
 //Zadanie z tresci rozdzialu
-//fs.stat('./cat.jpg', function(err, stats) {
-//    var statMode = new StatMode(stats);
-//    
-//    console.log(stats);
-//    console.log(statMode.toString());
-//});
+fs.stat('./cat.jpg', function(err, stats) {
+    var statMode = new StatMode(stats);
+    
+    console.log(stats);
+    console.log(statMode.toString());
+});
 
-//fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-//    
-//    console.log('Dane przed zapisem'.blue);
-//    console.log(data);
-//    
-//    fs.appendFile('./tekst.txt', '\n A tak wyglądają dane po zapisie', function(err) {
-//        if(err) throw err;
-//        console.log('Zapisano!');
-//        
-//        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-//        console.log('Dane po zapisie'.blue);
-//        console.log(data);
-//        });
-//    })
-//});
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    
+    console.log('Dane przed zapisem'.blue);
+    console.log(data);
+    
+    fs.appendFile('./tekst.txt', '\n A tak wyglądają dane po zapisie', function(err) {
+        if(err) throw err;
+        console.log('Zapisano!');
+        
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+        console.log('Dane po zapisie'.blue);
+        console.log(data);
+        });
+    })
+});
 
 //EVENT EMITTER
 var emitter = new EventEmitter();
