00214b07b09e8ba61b0a795e0657a1ba79b36c36
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-08-28 19:03:23 +0200

    Commit2

--- index.js
+++ index.js
@@ -2,21 +2,49 @@
 var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
 var fs = require('fs');
+var colors = require('colors');
 var StatMode = require('stat-mode');
 
 
+//READING FILE CONTENT
 
-fs.stat('./cat.jpg', function(err, stats) {
-    var statMode = new StatMode(stats);
+fs.readdir('./ToRead', 'utf-8', function(err, files) {
     
-    console.log(stats);
-    console.log(statMode.toString());
-});
-fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-    console.log(data);
+    console.log('Pliki w folderze: ', files);
+
+var array = new Array(files);
+
+    fs.writeFile('./File_list.txt', array, function(err) {
+        if(err) throw err;
+        console.log('Pliki zostały zapisane'.green);
+    });
 });
 
+//Zadanie z tresci rozdzialu
+//fs.stat('./cat.jpg', function(err, stats) {
+//    var statMode = new StatMode(stats);
+//    
+//    console.log(stats);
+//    console.log(statMode.toString());
+//});
 
+//fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+//    
+//    console.log('Dane przed zapisem'.blue);
+//    console.log(data);
+//    
+//    fs.appendFile('./tekst.txt', '\n A tak wyglądają dane po zapisie', function(err) {
+//        if(err) throw err;
+//        console.log('Zapisano!');
+//        
+//        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+//        console.log('Dane po zapisie'.blue);
+//        console.log(data);
+//        });
+//    })
+//});
+
+//EVENT EMITTER
 var emitter = new EventEmitter();
 emitter.on('beforeCommand', function(instruction) {
     console.log('You wrote: ' + instruction + ' trying to run command.')
