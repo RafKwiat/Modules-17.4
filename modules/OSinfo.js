d4884397b89d584f2e77e6d97e6c0f70588b9739
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-08-28 12:52:10 +0200

    Commit

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -12,11 +12,12 @@ function getOSinfo() {
         var release = os.release();
         var cpu = os.cpus()[0].model;
         var uptime = os.uptime();
-        var userInfo = os.userInfo();
+        var convertedTime = timeConvert(uptime);
+        var userInfo = os.userInfo(); 
         console.log('System: ', type);
         console.log('Release: ', release);
         console.log('CPU model: ', cpu);
-        console.log('Uptime:~ ', (uptime / 60).toFixed(0), 'min', times.timeConvert(sek));
+        console.log('Uptime: ',convertedTime);
         console.log('User name: ', userInfo.username);
         console.log('Home dir: ', userInfo.homedir);
         
--- modules/timeFormat.js
+++ modules/timeFormat.js
@@ -5,6 +5,6 @@ function timeConvert(sek) {
         var minutes = (hours - rHours) * 60;
         var rMinutes = Math.floor(minutes);
         var seconds = Math.round((minutes - rMinutes) * 60);
-        return num + " sekund = " + rHours + " godz. " + rMinutes + " min. " + seconds + " sek.";
+        return rHours + " godz. " + rMinutes + " min. " + seconds + " sek.";
 }
-exports.timeConvert = timeConvert;
+exports.timeConvert = convertedTime;
